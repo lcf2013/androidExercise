f6e99d22408285b8fec935e51b6c9d0925dedd3d
lcf2013 <lcf2013@yeah.net>
2015-12-02 18:32:56 +0800

    meaidRecorder,AudioRecord

--- media/src/main/java/com/zhenai/media/AudioRecordActivity.java
+++ media/src/main/java/com/zhenai/media/AudioRecordActivity.java
@@ -1,17 +1,195 @@
 package com.zhenai.media;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTimestamp;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AudioRecordActivity extends AppCompatActivity {
+
+    @Bind(R.id.satusTextView)
+    TextView satusTextView;
+    @Bind(R.id.startRecordBtn)
+    Button startRecordBtn;
+    @Bind(R.id.stopRecordBtn)
+    Button stopRecordBtn;
+    @Bind(R.id.playBtn)
+    Button playBtn;
+    @Bind(R.id.stopPlayBtn)
+    Button stopPlayBtn;
+
+    RecordAudioTask recordTask;
+    PlayAudio playTask;
+
+    private File recordFile;
+
+    boolean isRecording = false;
+    boolean isPlaying = false;
+
+    int frequency = 11025;
+    int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_record);
+        ButterKnife.bind(this);
+
+        stopRecordBtn.setEnabled(false);
+        playBtn.setEnabled(false);
+        stopPlayBtn.setEnabled(false);
+
+        File path = getFilesDir();
+        try {
+            recordFile = File.createTempFile("recording", ".pcm", path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
+    @OnClick({R.id.startRecordBtn, R.id.stopRecordBtn, R.id.play_btn, R.id.stopPlayBtn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.startRecordBtn:
+                record();
+                break;
+            case R.id.stopRecordBtn:
+                stopRecord();
+                break;
+
+            case R.id.play_btn:
+                play();
+                break;
+
+            case R.id.stopPlayBtn:
+                stopPlay();
+                break;
+        }
+    }
+
+    private void stopRecord() {
+
+    }
+
+    private void play() {
+        playBtn.setEnabled(true);
+        playTask = new PlayAudio();
+    }
+
+    private void record() {
+
+    }
+
+    private void stopPlay() {
+
+    }
+
+    class PlayAudio extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            isPlaying = true;
+            int bufferSize = AudioTrack.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+            short[] audioData = new short[bufferSize / 4];
+            try {
+                DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(recordFile)));
+                AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, frequency,
+                        channelConfiguration, audioEncoding, bufferSize, AudioTrack.MODE_STREAM);
+                audioTrack.play();
+
+                while (isPlaying && dis.available() > 0) {
+                    int i = 0;
+                    while (dis.available() > 0 && i < audioData.length) {
+                        audioData[i] = dis.readShort();
+                        i++;
+
+                    }
+                    audioTrack.write(audioData, 0, audioData.length);
+                }
+                dis.close();
+                //playBtn.setEnabled(false);
+                //stopPlayBtn.setEnabled(true);
+            } catch (Exception e) {
+
+            }
+
+
+            return null;
+        }
+    }
+
+    class RecordAudioTask extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            isRecording = true;
+
+            try {
+                DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(recordFile)));
+                int bufferSize = AudioRecord.getMinBufferSize(frequency,channelConfiguration,
+                        audioEncoding);
+                AudioRecord audioRecord=new AudioRecord(MediaRecorder.AudioSource.MIC,
+                        frequency,channelConfiguration,audioEncoding,bufferSize);
+                short[]buffer=new short[bufferSize];
+                audioRecord.startRecording();
+
+                int r=0;
+                while(isRecording){
+                    int bufferReadResult=audioRecord.read(buffer,0,bufferSize);
+                    for(int i=0;i<bufferReadResult;i++){
+                        dos.writeShort(buffer[i]);
+
+                    }
+                    publishProgress(new Integer(r));
+                    r++;
+                }
+                audioRecord.stop();
+                dos.close();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            satusTextView.setText(values[0].toString());
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            startRecordBtn.setEnabled(true);
+            stopRecordBtn.setEnabled(false);
+            playBtn.setEnabled(true);
+        }
+    }
 
 }
--- media/src/main/java/com/zhenai/media/MediaRecordActivity.java
+++ media/src/main/java/com/zhenai/media/MediaRecordActivity.java
@@ -1,12 +1,13 @@
 package com.zhenai.media;
 
+import android.content.ContentValues;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,8 +15,13 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.IOException;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MediaRecordActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener, View.OnClickListener {
 
+    @Bind(R.id.amplitudeText)
+    TextView amplitudeText;
     private TextView status;
     private Button start;
     private Button stop;
@@ -23,28 +29,29 @@ public class MediaRecordActivity extends AppCompatActivity implements MediaPlaye
     private Button finish;
 
 
-
     MediaRecorder recorder;
     MediaPlayer player;
     File audioFile;
+    private boolean isRecording;
+    private RecordAmplitudeTask recordAmplitudeTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_record);
+        ButterKnife.bind(this);
         initView();
     }
 
     private void initView() {
-        status=(TextView)findViewById(R.id.status);
-        start=(Button)findViewById(R.id.start);
-        stop=(Button)findViewById(R.id.stop);
-        play=(Button)findViewById(R.id.play);
-        finish=(Button)findViewById(R.id.finish);
+        status = (TextView) findViewById(R.id.status);
+        start = (Button) findViewById(R.id.start);
+        stop = (Button) findViewById(R.id.stop);
+        play = (Button) findViewById(R.id.play);
+        finish = (Button) findViewById(R.id.finish);
 
         stop.setEnabled(false);
         play.setEnabled(false);
-
 
 
         start.setOnClickListener(this);
@@ -53,20 +60,19 @@ public class MediaRecordActivity extends AppCompatActivity implements MediaPlaye
         finish.setOnClickListener(this);
 
 
-
     }
 
     @Override
     public void onCompletion(MediaPlayer mediaPlayer) {
-            play.setEnabled(true);
-            stop.setEnabled(false);
+        play.setEnabled(true);
+        stop.setEnabled(false);
         start.setEnabled(true);
         status.setText("ready");
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.start:
                 startRecord();
 
@@ -79,7 +85,6 @@ public class MediaRecordActivity extends AppCompatActivity implements MediaPlaye
                 stopRecord();
 
 
-
                 break;
 
             case R.id.finish:
@@ -87,7 +92,8 @@ public class MediaRecordActivity extends AppCompatActivity implements MediaPlaye
                 break;
 
 
-            default:break;
+            default:
+                break;
         }
     }
 
@@ -101,13 +107,13 @@ public class MediaRecordActivity extends AppCompatActivity implements MediaPlaye
     }
 
     private void startRecord() {
-        recorder=new MediaRecorder();
+        recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
         try {
-            audioFile=File.createTempFile("recording",".3gp",getFilesDir());
+            audioFile = File.createTempFile("recording", ".3gp", getFilesDir());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -128,13 +134,25 @@ public class MediaRecordActivity extends AppCompatActivity implements MediaPlaye
         play.setEnabled(false);
         stop.setEnabled(true);
         start.setEnabled(false);
+
+        isRecording=true;
+        recordAmplitudeTask=new RecordAmplitudeTask();
+        recordAmplitudeTask.execute();
+
+
     }
 
     private void stopRecord() {
+        isRecording=false;
+        recordAmplitudeTask.cancel(true);
+
         recorder.stop();
         recorder.release();
 
-        player=new MediaPlayer();
+        saveAudioFile();
+
+
+        player = new MediaPlayer();
 
         player.setOnCompletionListener(this);
 
@@ -155,5 +173,51 @@ public class MediaRecordActivity extends AppCompatActivity implements MediaPlaye
         play.setEnabled(true);
         stop.setEnabled(false);
         start.setEnabled(true);
+    }
+
+    /**
+     * 保存音频文件到mediastore
+     */
+    private void saveAudioFile() {
+        ContentValues values=new ContentValues();
+        values.put(MediaStore.MediaColumns.TITLE,"this is a audio record");
+        values.put(MediaStore.MediaColumns.DATE_ADDED,System.currentTimeMillis());
+        values.put(MediaStore.MediaColumns.DATA,audioFile.getAbsolutePath());
+        Uri newUri=getContentResolver().insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                values);
+    }
+
+    @Override
+    protected void onDestroy() {
+        ButterKnife.unbind(this);
+        if(isRecording)
+            stopRecord();
+        if(player.isPlaying())
+            player.stop();
+
+        super.onDestroy();
+
+    }
+    private class RecordAmplitudeTask extends AsyncTask<Void,Integer,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            while(isRecording){
+                try {
+                    Thread.sleep(500);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(recorder.getMaxAmplitude());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            amplitudeText.setText(values[0].toString());
+        }
     }
 }
