dff085bbb19e14915a0b451d8ec2bb8a10b79819
lcf2013 <lcf2013@yeah.net>
2016-03-11 18:53:51 +0800

    下拉刷新，折叠folding view

--- customview/src/main/java/com/zhenai/customview/MainActivity.java
+++ customview/src/main/java/com/zhenai/customview/MainActivity.java
@@ -1,44 +1,42 @@
 package com.zhenai.customview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private CustomView customView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        customView = (CustomView)findViewById(R.id.customView);
-        customView.setOnClickListener(new View.OnClickListener() {
+
+
+
+        findViewById(R.id.btn_move).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                customView.changeColor();
-                customView.invalidate();
+            public void onClick(View v) {
+                Intent  intent=new Intent(MainActivity.this,MoveGestureActivity.class);
+                startActivity(intent);
             }
         });
-    }
+        findViewById(R.id.btn_scale).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  intent=new Intent(MainActivity.this,ScaleGestureActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
