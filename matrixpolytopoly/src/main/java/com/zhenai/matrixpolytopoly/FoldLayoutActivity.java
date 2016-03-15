09b375a259305d1980a0d038e0703b720f162de9
lcf2013 <lcf2013@yeah.net>
2016-03-15 17:29:57 +0800

    qq侧滑菜单，folding layout

---
+++ matrixpolytopoly/src/main/java/com/zhenai/matrixpolytopoly/FoldLayoutActivity.java
@@ -0,0 +1,35 @@
+package com.zhenai.matrixpolytopoly;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author zhy http://blog.csdn.net/lmj623565791/
+ */
+public class FoldLayoutActivity extends Activity
+{
+	private FoldLayout mFoldLayout;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fold);
+
+		mFoldLayout = (FoldLayout) findViewById(R.id.id_fold_layout);
+	
+		/*mFoldLayout.post(new Runnable()
+		{
+
+			@SuppressLint("NewApi")
+			@Override
+			public void run()
+			{
+				ObjectAnimator.ofFloat(mFoldLayout, "factor", 1, 0, 1)
+						.setDuration(5000).start();
+			}
+		});*/
+
+	}
+}
